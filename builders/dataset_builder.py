99b4c19c579fe36c50b9a7bb45100db200d79947
Hendriks <thendriks@amc.intra>
2022-11-03 14:41:47 +0100

    set up input_file csv

--- builders/dataset_builder.py
+++ builders/dataset_builder.py
@@ -4,6 +4,7 @@ from torch.utils.data import Dataset
 from datasets.prosab_ct_dataset import ProsabCTDataset
 from pathlib import Path
 
+
 def build_dataset(dataset_name: str, **kwargs) -> Dataset:
     dataset = DATASET_CONSTRUCTORS[dataset_name](**kwargs)
     return dataset
@@ -14,11 +15,8 @@ def mock_image(**kwargs) -> MockImageDataset:
 
 
 def prosab_ct(**kwargs) -> ProsabCTDataset:
-    index_file_path = Path(kwargs['index_file_path'])
+    index_file_path = Path(kwargs["index_file_path"])
     return ProsabCTDataset(index_file_path=index_file_path)
 
 
-DATASET_CONSTRUCTORS = {
-    'mock_image': mock_image,
-    'prosab_ct': prosab_ct
-}
+DATASET_CONSTRUCTORS = {"mock_image": mock_image, "prosab_ct": prosab_ct}
