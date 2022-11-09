704cc342f81b4203c779dbca8df2a1780b131e31
tomhend <tom.hendriks94@gmail.com>
2022-11-09 17:10:24 +0100

    hopefully final commit for rtd

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,6 @@ import os
 import sys
 sys.path.insert(0, '../..')
 sys.path.insert(0, '..')
-
-import sphinx_rtd_theme
 # -- Project information -----------------------------------------------------
 
 project = 'base_project'
@@ -31,8 +29,7 @@ author = 'Author'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'sphinx.ext.todo',
-    'sphinx_rtd_theme'
+    'sphinx.ext.todo'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -56,7 +53,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
