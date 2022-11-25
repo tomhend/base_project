bd8fa66c735b22a771de26d95c33e8b4a3677221
tomhend <tom.hendriks94@gmail.com>
2022-11-25 12:19:31 +0100

    more merge conflicts solved

--- trainers/base_trainer.py
+++ trainers/base_trainer.py
@@ -81,11 +81,7 @@ class BaseTrainer:
         for i, (_input, label) in enumerate(tqdm(dataloader)):
             step = epoch * len(dataloader) + i
 
-<<<<<<< HEAD
             _input = _input.to(self.device)
-=======
-            # _input = _input.to(self.device) may cause memory issues with large datasets
->>>>>>> origin/main
             label = label.to(self.device)
             self.optimizer.zero_grad()
 
